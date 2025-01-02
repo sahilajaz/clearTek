90365bc86c071456cdb3dc0d6e1353fd54f10635
sahilajaz <sahilajazw@gmail.com>
2025-01-02 19:46:35 +0530

    changes made to form

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,11 +11,11 @@ const Footer = () => {
     <footer className="mt-16">
       <section className="form-section mx-20">
         <div className='bg-[url("/formimg.png")] w-full h-[610px] '>
-          <div className="w-full h-full flex flex-col items-center pl-80 pt-32  gap-10">
-            <h1 className="text-white font-semibold text-4xl mr-20">
+          <div className="w-full h-full flex flex-col   pl-[600px] pt-32  gap-10">
+            <h1 className="text-white font-semibold text-4xl mx-5">
               Get In Touch With Us!
             </h1>
-             <form className="grid grid-cols-2 gap-8 full">
+             <form className="grid grid-cols-2 gap-8 w-full px-5">
               <input className="inputFiled w-full py-3 px-1" placeholder="First Name"/>
               <input className="inputFiled w-full py-3 px-1" placeholder="Last Name"/>
               <input className="inputFiled w-full py-3 px-1" placeholder="Your Email"/>
