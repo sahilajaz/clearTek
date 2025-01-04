53fe5cfe68e286573cdd0b368ccd8a7c0cb429ee
sahilajaz <sahilajazw@gmail.com>
2025-01-04 19:47:16 +0530

    changes

--- components/OurServiceSection.tsx
+++ components/OurServiceSection.tsx
@@ -6,7 +6,7 @@ const OurServiceSection = () => {
     <section className='w-full  border-t-4 border-[#0060de]'>
         <div className='w-full text-center mt-10'>
         <h1 className='text-[#1b2635] text-[30px] sm:text-[47px]'>Our Services</h1>
-        <div className='cards mx-10 lg:mx-16  2xl:mx-72'>
+        <div className='cards mx-24 lg:mx-16  2xl:mx-72'>
           <ServiceCard/>
         </div>
         </div>
