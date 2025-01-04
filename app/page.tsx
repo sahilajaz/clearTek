21e6bc28a1e2dbeffb9f3646d6bf71793c3c28a7
sahilajaz <sahilajazw@gmail.com>
2025-01-04 12:19:21 +0530

    vw

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="w-full h-full">
-      <div className="relative w-[500px] sm:w-full h-[800px]">
+      <div className="relative w-[411px] sm:w-full h-[800px]">
         <Image
           src="/heroimage.jpg"
           alt="hero-image"
