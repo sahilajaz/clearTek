03d6358e9c894bb550e1d6abc726dbb1140115a3
sahilajaz <sahilajazw@gmail.com>
2025-01-06 17:29:31 +0530

    changes amde

--- app/pages/(secured)/layout.tsx
+++ app/pages/(secured)/layout.tsx
@@ -1,6 +1,7 @@
 
 import Layout  from "../../(main)/layout"
 //@ts-expect-error: This is intentional
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ children }) => {
   return (
     <Layout showContactUs={true}>
